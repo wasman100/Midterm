70d8add950c7273363d5120c06cd556abbdad955
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-23 10:29:30 -0400

    updated karel with out variables except in for loop

--- src/Karel.java
+++ src/Karel.java
@@ -3,56 +3,33 @@ import stanford.karel.SuperKarel;
 
 public class Karel extends SuperKarel{
 	public void run() {
-		sizeOfBox();
-	}
-
-	private void sizeOfBox() {
-		// TODO Auto-generated method stub
-		int countRows = 0;
-		int countCol = 0;
-		while(frontIsClear()) {
-			countRows++;
-			move();
-		}
-		while(notFacingNorth()) {
-			turnLeft();
-		}
-		while(frontIsClear()) {
-			countCol++;
-			move();
-		}
-		StartPlaceingBeepers( countRows,  countCol);
-
-	}
-
-	private void StartPlaceingBeepers(int countRows, int countCol) {
-		// TODO Auto-generated method stub
-		while(notFacingWest()) {
-			turnLeft();
-		}
 		move();
 		turnLeft();
 		move();
-		putBeeper();
-		while(notFacingWest()) {
-			turnLeft();
-		}
-		for(int l = 0; l < 2; l++) {
-		for(int i = 0; i<(countRows-2); i++) {
-			move();
-			putBeeper();
+		while(notFacingEast()) {
+			turnLeft();		}
+		StartPlaceingBeepers();
 		}
 
-			turnLeft();
-
-		for(int x =0; x < (countCol-2); x++) {
-			move();
+	private void StartPlaceingBeepers() {
+		// TODO Auto-generated method stub
+		for(int i =0; i<4; i++) {
+		while(frontIsClear()) {
 			if(noBeepersPresent()) {
 				putBeeper();
 			}
-
+			move();
 		}
+		backup();
 		turnLeft();
 		}
 	}
+	private void backup() {
+		// TODO Auto-generated method stub
+		turnAround();
+		move();
+		turnAround();
+	}
+
+
 }
