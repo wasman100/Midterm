b59ae48e68a4052d3a48c56d91f95175e88b8528
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-23 11:24:21 -0400

    this programs gets the largest and second largest integers entered

---
+++ src/largestSecondLargest.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import acm.program.*;
+
+public class largestSecondLargest extends ConsoleProgram  {
+
+	public void run() {
+		println("This program finds the two largest integers in a list. \n Enter Values, one per line, using a 0 to signal the end of the list");
+		ArrayList<Integer> listint = new ArrayList<Integer>();
+		listint=gettingintegers();
+		println(listint.get(0));
+		println(listint.get(1));
+	}
+
+	private ArrayList<Integer> gettingintegers() {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> listint = new ArrayList<Integer>();
+		while(true) {
+			int a = readInt("?");
+			if (a==0) {
+				break;
+			}
+			listint.add(a);
+		}
+		Collections.reverse(listint);
+		return listint;
+		
+	}
+}
